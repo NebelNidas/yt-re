4ac317d06f69fd39e9d060994597f9bc750c4379
NebelNidas <burnerjulian@gmail.com>
2023-03-26 13:38:35 +0200

    Add package Javadoc for `com.google.apps.tiktok`

---
+++ src/packageDocs/java/com/google/apps/tiktok/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * `tiktok` is an internal Google library used by many of their apps.
+ * It was written and named like that before the social network existed
+ * and is absolutely unrelated.
+ *
+ * See <a href="https://www.reddit.com/r/mAndroidDev/comments/l14tly/comgoogleappstiktok/">r/mAndroidDev</a>.
+ */
+
+package com.google.apps.tiktok;
